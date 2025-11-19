d8f36f3ac22967570d41abc8c87714b5b86d6109
yanbin <yanbin@ubanquan.cn>
2025-11-19 15:07:52 +0800

    update: run start

--- run.sh
+++ run.sh
@@ -27,8 +27,20 @@ echo "
 +----------------------------------------------------------------------
 " >> "$LOG_FILE"
 
+# 构建 classpath：包含 jar 文件和 lib 目录下的所有依赖
+if [ -d "lib" ] && [ "$(ls -A lib/*.jar 2>/dev/null)" ]; then
+    CLASSPATH="./bin/maple.jar:$(find lib -name "*.jar" | tr '\n' ':')"
+    # 移除末尾的冒号
+    CLASSPATH="${CLASSPATH%:}"
+else
+    CLASSPATH="./bin/maple.jar"
+fi
+
 # 后台运行 Java 服务，并将输出重定向到日志文件
-nohup ./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start >> "$LOG_FILE" 2>&1 &
+nohup ./jdk/jre/bin/java -cp "$CLASSPATH" -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start >> "$LOG_FILE" 2>&1 &
+
+# 后台运行 Java 服务，并将输出重定向到日志文件
+# nohup ./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start >> "$LOG_FILE" 2>&1 &
 
 # 获取进程ID
 PID=$!
--- start.sh
+++ start.sh
@@ -3,4 +3,15 @@ echo "
 |                   冒险岛079 FOR CentOS/Ubuntu/Debian
 +----------------------------------------------------------------------
 "
-./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
+# ./jdk/jre/bin/java -cp ./bin/maple.jar -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
+
+# 构建 classpath：包含 jar 文件和 lib 目录下的所有依赖
+if [ -d "lib" ] && [ "$(ls -A lib/*.jar 2>/dev/null)" ]; then
+    CLASSPATH="./bin/maple.jar:$(find lib -name "*.jar" | tr '\n' ':')"
+    # 移除末尾的冒号
+    CLASSPATH="${CLASSPATH%:}"
+else
+    CLASSPATH="./bin/maple.jar"
+fi
+
+./jdk/jre/bin/java -cp "$CLASSPATH" -server -DhomePath=./config/ -DscriptsPath=./scripts/ -DwzPath=./scripts/wz -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=512m -XX:MaxNewSize=512m server.Start
